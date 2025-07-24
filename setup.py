3b0ca6ae0efac7367e1919b2aef8ebb159904c97
auzigog <testtt8181@gmail.com>
2025-07-24 21:38:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 from setuptools import setup
+import os
 
-print("RCE capabilities PoC - cygut7")
+print("[*] This is a harmless PoC file to demonstrate RCE capabilities - cygut7")
 
+# Cross-platform path to user home dir
+home_dir = os.path.expanduser("~")
+
+# Full file path
+proof_path = os.path.join(home_dir, "RCE_PROOF.txt")
+
+# Write PoC file in home dir
+with open(proof_path, "w") as f:
+    f.write("This is a harmless PoC file to demonstrate RCE capabilities - cygut7")
+
+# Basic harmless setup config
 setup(
     name="jinja-bootstrap",
     version="0.3.0",
+    description="PoC hijacked package",
+    author="cygut7",
+    py_modules=[],
 )
